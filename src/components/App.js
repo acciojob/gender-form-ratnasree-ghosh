d68e4c72a25529bac187d05fc09ce38574f4c8a6
Ratnasree Ghosh <86732136+ratnasree-ghosh@users.noreply.github.com>
2023-06-18 07:30:24 +0000

    1st commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React , {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [code, setCode] = useState("dress");
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h2>Select your gender:</h2>
+        <input name="gender" id="male" type="radio" onChange={()=>setCode("shirt")}/>
+        <label for="male">Male</label>
+        <input name="gender"  id="female" type="radio" onChange={()=>setCode("dress")}/>
+        <label for="female">Female</label>
+
+        <h2>Select your {code} size:</h2>
+        <select>
+          <option  value="2">2</option>
+          <option  value="4">4</option>
+          <option value="6">6</option>
+        </select>
     </div>
   )
 }
